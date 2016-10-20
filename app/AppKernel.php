8f6d6673f9755ede37a18704319d933054f5825f
alucard371 <test8q@gmail.com>
2016-10-20 14:42:45 +0200

    removed unused css selectors from style.css

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -7,7 +7,7 @@ class AppKernel extends Kernel
 {
     public function registerBundles()
     {
-        $bundles = [
+        $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -23,7 +23,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
 	        new UserBundle\UserBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-        ];
+        );
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
